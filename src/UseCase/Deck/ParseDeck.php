83d1dd21fd1c87a7a80361244922fecf895721f9
Tim Neumann <tim.neumann1@gmail.com>
2017-11-04 21:02:51 +0100

    updated dependency

--- src/UseCase/Deck/ParseDeck.php
+++ src/UseCase/Deck/ParseDeck.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace vonZnotz\MtgDeckParser\UseCase\Deck;
 
 use vonZnotz\MtgDeckParser\Service\Deck\CardDataUpdater;
-use vonZnotz\MtgDeckParser\Service\Deck\DeckCollection;
 use vonZnotz\MtgDeckParser\UseCase\AbstractUseCase;
 use vonZnotz\MtgDeckParser\UseCase\Deck\Request\ParseDeckRequest;
 use vonZnotz\MtgDeckParser\UseCase\Deck\Response\ParseDeckResponse;
@@ -17,7 +16,7 @@ class ParseDeck extends AbstractUseCase
 
     public function __construct(CardDataUpdater $cardDataUpdater)
     {
-        $this->cardDataUpdater = new CardDataUpdater();
+        $this->cardDataUpdater = $cardDataUpdater;
     }
 
     public function run(ParseDeckRequest $request, ParseDeckResponse $response): ParseDeckResponse
