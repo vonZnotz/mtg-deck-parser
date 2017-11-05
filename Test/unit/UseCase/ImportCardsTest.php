1d6a72b27f8e21a491248f9766723f9528e92d2c
Tim Neumann <tim.neumann1@gmail.com>
2017-11-05 21:19:47 +0100

    updated import database

--- Test/unit/UseCase/ImportCardsTest.php
+++ Test/unit/UseCase/ImportCardsTest.php
@@ -16,12 +16,12 @@ class ImportCardsTest extends TestCase
 {
     public function testImport()
     {
-        $zipFilename = dirname(__FILE__) . "/../../../app/cache/AllCards.json.zip";
+        $zipFilename = dirname(__FILE__) . "/../../../app/cache/AllSets.json.zip";
         if (file_exists($zipFilename)) {
             unlink($zipFilename);
         }
 
-        $jsonFilename = dirname(__FILE__) . "/../../../app/cache/AllCards.json";
+        $jsonFilename = dirname(__FILE__) . "/../../../app/cache/AllSets.json";
         if (file_exists($jsonFilename)) {
             unlink($jsonFilename);
         }
--- src/Service/Cards/ExternalCardDatabaseProvider.php
+++ src/Service/Cards/ExternalCardDatabaseProvider.php
@@ -7,14 +7,14 @@ namespace vonZnotz\MtgDeckParser\Service\Cards;
 
 class ExternalCardDatabaseProvider implements ExternalCardDatabaseProviderInterface
 {
-    const EXTERNAL_DATABASE_URL = 'https://mtgjson.com/json/AllCards.json.zip';
+    const EXTERNAL_DATABASE_URL = 'https://mtgjson.com/json/AllSets.json.zip';
 
     private $zipFilename;
     private $targetDirectory;
 
     public function __construct()
     {
-        $this->zipFilename = dirname(__FILE__) . "/../../../app/cache/AllCards.json.zip";
+        $this->zipFilename = dirname(__FILE__) . "/../../../app/cache/AllSets.json.zip";
         $this->targetDirectory = dirname(__FILE__) . "/../../../app/cache";
     }
 
