1ad3dc9800106d4808831d6b3f25ab610f489b95
Tim Neumann <tim.neumann1@gmail.com>
2017-11-06 07:53:42 +0100

    using new internal database instead of the gatherer website

--- src/Service/Cards/CardDataProvider.php
+++ src/Service/Cards/CardDataProvider.php
@@ -8,26 +8,16 @@ class CardDataProvider implements CardDataProviderInterface
 {
     public function updateCardInformationByMultiverseId(DeckItem $deckItem): void
     {
-        $baseUrl = 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' . $deckItem->getMultiverseId();
-        $html = file_get_contents($baseUrl);
+        $mulitverseId = $deckItem->getMultiverseId();
+        $jsonFile = __DIR__ . "/../../../app/cache/cards/" . $mulitverseId . ".json";
 
-        $deckItem->setName($this->getCardName($html));
+        $jsonData = file_get_contents($jsonFile);
+        $cardItem = json_decode($jsonData);
+
+        $deckItem->setName($cardItem->name);
 
         if ($deckItem->getAmount() === null) {
             $deckItem->setAmount(1);
         }
-    }
-
-    private function getCardName(string $html):string
-    {
-        $startString = 'Card Name:</div>';
-        $start = strpos($html, 'Card Name:</div>');
-        $start += strlen($startString);
-        $startString2 = '<div class="value">';
-        $start2 = strpos($html, $startString2, $start) + strlen($startString2);
-
-        $startHtml = trim(substr($html, $start2));
-        $cardName = substr($startHtml, 0, strpos($startHtml, '</div>'));
-        return $cardName;
     }
 }
