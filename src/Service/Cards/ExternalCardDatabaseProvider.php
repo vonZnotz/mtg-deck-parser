72dbf67af8dbebe9ea31e363352e64877bcf6af5
Tim Neumann <tim.neumann1@gmail.com>
2017-11-10 15:05:23 +0100

    removed comment

--- src/Service/Cards/ExternalCardDatabaseProvider.php
+++ src/Service/Cards/ExternalCardDatabaseProvider.php
@@ -34,8 +34,6 @@ class ExternalCardDatabaseProvider implements ExternalCardDatabaseProviderInterf
 
     public function importDatabase()
     {
-        // mulitverseid.json
-        // 73937.json
         $jsonContent = file_get_contents($this->targetDirectory . "/AllSets.json");
         $data = json_decode($jsonContent);
 
@@ -43,6 +41,7 @@ class ExternalCardDatabaseProvider implements ExternalCardDatabaseProviderInterf
             $jsonCardData = new \stdClass();
             $jsonCardData->set_name = $setItem->name;
             $jsonCardData->set_code = $setItem->code;
+
             foreach ($setItem->cards as $cardItem) {
                 $jsonCardData = (object) array_merge((array) $jsonCardData, (array) $cardItem);
                 if (!property_exists($cardItem, "multiverseid")) {
